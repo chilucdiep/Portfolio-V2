713c4c938312f8ad89970830a3864198b6e187be
Chi-Luc <chilucdiep@hotmail.com>
2021-09-06 16:49:21 -0400

    Social media links

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,9 @@
 import styles from "./Navbar.module.scss";
 
 import Logo from "../../images/Logo.svg";
+import GithubIcon from "../../images/GithubIcon.png";
+import LinkedinIcon from "../../images/LinkedinIcon.png";
+import { MAIL_ICON } from "../Global/Icons";
 
 export default function Navbar() {
   return (
@@ -12,15 +15,31 @@ export default function Navbar() {
             <a href="/">Work</a>
           </li>
           <li>
-            <a href="/">About</a>
+            <a href="/about">About</a>
           </li>
         </ul>
       </div>
       <div className={styles.SocialLinks}>
         <ul>
-          <li>In</li>
-          <li>Git</li>
-          <li>M</li>
+          <li>
+            <a href="https://www.linkedin.com/in/chi-luc-diep/" target="_blank">
+              <img
+                src={LinkedinIcon}
+                alt="Github"
+                className={styles.NavIcon}
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/chilucdiep" target="_blank">
+              <img
+                src={GithubIcon}
+                alt="Github"
+                className={styles.NavIcon}
+              ></img>
+            </a>
+          </li>
+          <li><a href='mailto:chilucdiep@hotmail.com'>{MAIL_ICON}</a></li>
         </ul>
       </div>
     </div>
