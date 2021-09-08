6227742a8e7430fd2911f0a099295f8f1d418c61
Chi-Luc <chilucdiep@hotmail.com>
2021-09-08 19:51:39 -0400

    Button animation

--- src/components/Global/Button/index.tsx
+++ src/components/Global/Button/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Button.module.scss";
-import about from "../../Home/Home.module.scss";
+import home from "../../Home/Home.module.scss";
 
 interface Props {
   label: string;
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Button({ label, icon, onClick }: Props) {
   return (
-    <a className={`${styles.Button} ${about.AboutButton}`} onClick={onClick}>
+    <a className={`${styles.Button} ${home.HeroButton} ${home.WorkButton}`} onClick={onClick}>
       <span>{icon}</span>
       {label}
     </a>
