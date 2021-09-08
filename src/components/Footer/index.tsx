503deb38eb47ec81f99f76c3e8bd03af00dbb539
Chi-Luc <chilucdiep@hotmail.com>
2021-09-07 20:17:46 -0400

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,38 @@
-import styles from "./Navbar.module.scss";
+import styles from "./Footer.module.scss";
 
-import Logo from "../../images/Logo.svg";
 import GithubIcon from "../../images/GithubIcon.png";
 import LinkedinIcon from "../../images/LinkedinIcon.png";
 import { MAIL_ICON } from "../Global/Icons";
 
 export default function Footer() {
   return (
-      <div>ok</div>
+    <div className={styles.Footer}>
+        <div className={styles.SocialLinks}>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/chi-luc-diep/" target="_blank">
+              <img
+                src={LinkedinIcon}
+                alt="Github"
+                className={styles.NavIcon}
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/chilucdiep" target="_blank">
+              <img
+                src={GithubIcon}
+                alt="Github"
+                className={styles.NavIcon}
+              ></img>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:chilucdiep@hotmail.com">{MAIL_ICON}</a>
+          </li>
+        </ul>
+      </div>
+      <p>Designed and built by me :)</p>
+    </div>
   );
 }
