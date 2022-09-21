4b9c5bdcf8044637e04a966309af72a36f77f168
chiludiep <chiludiep@hotmail.com>
2022-09-21 14:41:26 -0400

    Img

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,7 +8,7 @@ import ColoredByte from "../../images/ColoredByte.svg";
 import Pratt from "../../images/Pratt.svg";
 import Wave from "../../images/Wave.svg";
 import MissionImg from "../../images/MissionImg.png";
-import { TechnologiesList, ToolsList, SkillsList } from  './SkillsList'
+import { TechnologiesList, ToolsList, SkillsList } from "./SkillsList";
 
 export default function AboutContainer() {
   return (
@@ -29,32 +29,48 @@ export default function AboutContainer() {
           <h2>My journey in tech</h2>
           <p>
             My name is Chi-Luc and I am a Software Developer based in 📍
-            Montreal, Canada. I’m currently working as a Frontend Developer at Poka.
+            Montreal, Canada. I’m currently at{" "}
+            <a
+              href="https://www.poka.io"
+              target="_blank"
+              style={{ color: "#6773FF" }}
+              rel="noreferrer"
+            >
+              <strong>Poka</strong>
+            </a>{" "}
+            where I have a direct impact on the business' metrics as a Frontend
+            Developer working on the pricing plans & web admin.
           </p>
           <p>
-            From delivering insightful usage analytics to Coveo's clients as a UX Designer, 
-            to impacting directly Poka's business metrics as a Frontend Developer working on the pricing plans & web admin, 
-            I’m always looking for ways to improve the user experience by tackling interesting challenges.
+            Previously, I worked as a UX Designer at{" "}
+            <a
+              href="https://www.coveo.com"
+              target="_blank"
+              style={{ color: "#6773FF" }}
+              rel="noreferrer"
+            >
+              <strong>Coveo</strong>
+            </a>{" "}
+            where I delivered usage analytics solutions to Coveo's clients. From
+            revamping a master component in the design system, to designing a
+            Data Health feature, I’m always looking for ways to improve the user
+            experience by tackling interesting challenges.
           </p>
           <p>
-            I spent 3 years as a Electrical/Mechanical Engineering Student at Polytechnique
-            Montreal. Driven by curiosity, I discovered the magic of web
-            development and went on the path of self-study with the help of
-            amazing mentors and online ressources.
-          </p>
-          <p>
-            In my free time, you can find me reading, weightlifting and solving
-            problems through Jiu-Jitsu and bouldering.
+            I spent 3 years as a Electrical/Mechanical Engineering Student at
+            Polytechnique Montreal. Driven by curiosity, I discovered the
+            ✨magic✨ of web development and went on the path of self-study with
+            the help of amazing mentors, internships and online ressources.
           </p>
         </div>
       </div>
       <div className={styles.SkillGrid}>
-        <ListComponent listType='Technologies' />
-        <ListComponent listType='Tools' />
-        <ListComponent listType='Skills' />
+        <ListComponent listType="Technologies" />
+        <ListComponent listType="Tools" />
+        <ListComponent listType="Skills" />
       </div>
       <div className={styles.Experience}>
-        <h2>I create products for your business needs.</h2>
+        <h2>I create products that have an impact.</h2>
         <div className={styles.LogoGrid}>
           <img src={Coveo} alt="Coveo Logo" />
           <img src={Billmate} alt="Billmate Logo" className={styles.Smaller} />
@@ -78,28 +94,32 @@ export default function AboutContainer() {
             </p>
             <ul>
               <li>
-                <h4>Collaboration & Ownership</h4>
+                <h4>Team work makes dream work</h4>
                 <p>
-                  I believe the feeling of ownership shared among team members
-                  promotes a more proactive approach. Working as a team means
-                  utilizing different people’s strenghts to create a solution
-                  and have an impact.
+                  I deeply value collaboration, clear & transparent
+                  communication and good team chemistry as they allow me to
+                  thrive in my work. To me, Working as a team means utilizing
+                  different people’s strenghts to create a solution and have an
+                  impact.
                 </p>
               </li>
               <li>
-                <h4>Diversity & Open-mindedness</h4>
+                <h4>Quality & Attention to detail</h4>
                 <p>
-                  To me, diversity means the sharing of unique experiences,
-                  backgrounds and different point of views to bring new
-                  perspectives and constant growth.
+                  I care about the user experience as much as the developer
+                  experience. It is important for me to create maintainable and
+                  scalable code, As a Frontend developer, this pairs well with
+                  my modern CSS skills and a strong understanding of UX
+                  fundamentals.
                 </p>
               </li>
               <li>
-                <h4>Growth Mindset </h4>
+                <h4>Growth mindset </h4>
                 <p>
-                  Learning is my main focus. I love trying new things and
-                  taking on interesting challenges. I also believe that
-                  mentorship, feedback and transparency is essential for growth.
+                  I am driven, curious and a fast learner. I love trying new
+                  things and challenging myself to keep growing and learning. I
+                  also believe that mentorship, feedback and transparency is
+                  essential for growth.
                 </p>
               </li>
             </ul>
@@ -114,36 +134,34 @@ export default function AboutContainer() {
 function ListComponent({ listType }: any) {
   let listMarkup;
 
-  if (listType === 'Technologies') {
-    listMarkup = TechnologiesList?.map(skill => (
+  if (listType === "Technologies") {
+    listMarkup = TechnologiesList?.map((skill) => (
       <li key={skill}>
         <p>{skill}</p>
       </li>
-    ))
+    ));
   }
 
-  if (listType === 'Tools') {
-    listMarkup = ToolsList?.map(skill => (
+  if (listType === "Tools") {
+    listMarkup = ToolsList?.map((skill) => (
       <li key={skill}>
         <p>{skill}</p>
       </li>
-    ))
+    ));
   }
 
-  if (listType === 'Skills') {
-    listMarkup = SkillsList?.map(skill => (
+  if (listType === "Skills") {
+    listMarkup = SkillsList?.map((skill) => (
       <li key={skill}>
         <p>{skill}</p>
       </li>
-    ))
+    ));
   }
 
   return (
     <div>
       <h4>{listType}</h4>
-      <ul>
-      {listMarkup}
-      </ul>
+      <ul>{listMarkup}</ul>
     </div>
-  )
+  );
 }
